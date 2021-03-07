6bfea02055aff5e58b1cb6ecbe1fbd04df114e10
DESKTOP-FU6I8N3\spite <spiteriemma@gmail.com>
2021-03-07 23:25:29 +0100

    test finished

--- EmmaSpit_DSA_Assign1/AbstractDataType .cs
+++ EmmaSpit_DSA_Assign1/AbstractDataType .cs
@@ -8,14 +8,9 @@ namespace EmmaSpit_DSA_Assign1
     {
         void SaveArray(T element);
 
-        /// <summary>
-        /// Counts the number of times that the element appears in the Bag
-        /// </summary>
-        /// <param name="element">The element to be counted</param>
-        /// <returns>The number of times that the element was found</returns>
         int CountWords(T element);
 
-        
+        void UniqueWords(T element);
 
         IEnumerable<T> ListAllElements();
     }
