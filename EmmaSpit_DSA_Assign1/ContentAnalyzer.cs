db9fa8bc6f8481d9a4881d20a01d36f38461a3f4
DESKTOP-FU6I8N3\spite <spiteriemma@gmail.com>
2021-03-07 23:17:36 +0100

    Assign1 - Finished

--- EmmaSpit_DSA_Assign1/ContentAnalyzer.cs
+++ EmmaSpit_DSA_Assign1/ContentAnalyzer.cs
@@ -9,7 +9,8 @@ namespace EmmaSpit_DSA_Assign1
         //Input all the methods - Working 
 
         private Dictionary<T, int> itemArray =
-           new Dictionary<T, int>();       // T is the element that I am storing, int is the number of times that it "appears"
+           new Dictionary<T, int>();
+
 
         //•ToArray () : T[ ] - saved in array || display from array? 
         public void SaveArray(T element)
@@ -21,8 +22,6 @@ namespace EmmaSpit_DSA_Assign1
             }
             else
             {
-                // The element does not exist already
-                // The new element has a count of 1
                 itemArray.Add(element, 1);
             }
         }
@@ -33,6 +32,7 @@ namespace EmmaSpit_DSA_Assign1
         {
             return itemArray[element];
         }
+
 
         //Display
         public IEnumerable<T> ListAllElements()
@@ -46,45 +46,17 @@ namespace EmmaSpit_DSA_Assign1
         {
             if (itemArray.ContainsKey(element))
             {
-                itemArray.Add(element,1);
+                itemArray.Add(element, 1);
             }
             else
             {
                 itemArray[element] =
                     itemArray[element] + 1;
-                
+
             }
         }
         /*
             • DataSimilarity(T data) :  int - return int[??]
-      */
-
+        */
     }
-
 }
-
-/*
-      
-
-          public AbstractDataType<T> Union(AbstractDataType<T> otherBag)
-          {
-              ContentAnalyzer<T> output = new ContentAnalyzer<T>();
-
-              foreach (T getKey in itemCountDictionary.Keys)
-              {
-                  // get all elements one by one
-                  output.itemCountDictionary.Add(getKey, itemCountDictionary[getKey]);
-              }
-              // fill up all the elements contained in this dictionary to the output's dictionary
-
-              // add one by one all the elements contained in the other bag
-              foreach (T element in otherBag.ListAllElements())
-              {
-                  output.Add(element);
-              }
-
-              return output; // implicit type casting, since BagDS is and IBagADT
-         */
-
-
-
